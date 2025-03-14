421a60e57290523fa8052c5698ec8df3c125ee5f
gee989185 <gee020915@gmail.com>
2025-03-14 10:46:33 +0000

    Week1.py 파일 추가

--- Week1.py
+++ Week1.py
@@ -1,1 +1,11 @@
 print("hello World!")
+print("Hello Jichan")
+# Look for the Git icon in your Tools section (where Console and Shell are).
+# Click the gear (settings) icon in the upper-right corner of the Git pane.
+# Click "Connect to GitHub" and log in with your GitHub credentials.
+# When prompted, give Replit access to your repositories by selecting "All repositories" and clicking "Save".
+# Fill in your repository details:
+# Repository name
+# Description (optional)
+# Choose Public or Private
+# Click "Create Repository on GitHub"
